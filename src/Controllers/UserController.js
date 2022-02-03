a0fa61e60f889741c830476b83bd567e5bd91b0f
Emerson <emerson.miranda@globalhitss.com.br>
2022-02-03 19:02:42 -0300

    Oi

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -4,10 +4,10 @@ exports.post = (req, res, next) => {
 
 exports.put = (req, res, next) => {
     let id = req.params.id;
-    res.status(201).send('Rota PUT com ID! --> ${id}');
+    res.status(201).send('Rota PUT com ID! OI --> ${id}');
  };
  
- exports.delete = (req, res, next) => {
+ exports.delete = (req, res, next) ====> {
     let id = req.params.id;
     res.status(200).send('Rota DELETE com ID aqui! --> $(id)');
  };
