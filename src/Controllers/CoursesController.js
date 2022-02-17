cc76270997695e7906d0a324fe2d46b5e8feb82a
Emerson <emerson.miranda@globalhitss.com.br>
2022-02-17 10:07:46 -0300

    stream-video

--- src/Controllers/CoursesController.js
+++ src/Controllers/CoursesController.js
@@ -1,3 +1,4 @@
+const fs = require ('fs');
 const db = require('../../db');
 // operação post: criar cursos
 exports.post = (req, res) => { // req, res, next são atributos basicos de toda requisição
@@ -45,8 +46,40 @@ exports.getById = (req, res) => {
 };
 
 exports.videoUpload = async (req, res) => {
-  console.log (req.file)
   const { id } = req.params;
   await db('courses').where({ id }).first().update({ videoPath: req.file.path });
   res.send('uploaded successfully');
+};
+
+exports.getVideo = async (req, res) => {
+  const { id } = req.params;
+  const movieFile = await db('courses').where({ id }).first(); //onde está salvo o video
+  console.log(movieFile.videoPath);
+  fs.stat(movieFile.videoPath, (err, stats) => {
+    if (err) {
+      console.log(err);
+      return res.status(404).end('<h1>Movie Not Found</h1>');
+    }
+    // variaveis necessarias para montar o chunk header corretamente
+    const { range } = req.headers;
+    const { size } = stats;
+    const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+    const end = size - 1;
+    const chunkSize = (end - start) + 1;
+    // definindo headers de chunk
+    res.set({
+      'Content-Range': `bytes ${start}-${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-type': 'video/mp4',
+    });
+    // é importante usar status 206 - Partial Content para o streaming funcionar 
+    res.status(206);
+    //utilizando ReadStream no Node.js
+    //Ele vai ler um arquivo e envia-lo em partes via stream.pipe()
+    const stream = fs.createReadStream(movieFile.videoPath, { start, end});
+    stream.on('open', () => stream.pipe(res));
+    stream.on('error', (streamErr) => res.end(streamErr));
+    return null;
+  });
 };
--- src/Routes/CoursesRoute.js
+++ src/Routes/CoursesRoute.js
@@ -1,11 +1,16 @@
+const fs = require('fs');
 const CoursesController = require('../Controllers/CoursesController');
 const upload = require('../../common');
 
 module.exports = (app) =>{
+   app.get('/', (req, res) => {
+      fs.readFile('./index.html', (err, html) => res.end(html));
+   });
    app.post('/course', CoursesController.post);
    app.put('/course/:id', CoursesController.put);
    app.delete('/course/:id', CoursesController.delete);
    app.get('/courses', CoursesController.get);
    app.get('/course/:id', CoursesController.getById);
    app.post('/courses/upload/:id', upload.single('video'), CoursesController.videoUpload);
+   app.get('/courses/:id', CoursesController.getVideo);
 };
