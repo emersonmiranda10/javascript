2ee3cc622382dace61e4157e200f83735ac0bc9c
Emerson <emerson.miranda@globalhitss.com.br>
2022-02-23 15:40:32 -0300

    Progress

--- src/Controllers/CoursesController.js
+++ src/Controllers/CoursesController.js
@@ -39,22 +39,56 @@ exports.getById = (req, res) => {
       id,
   }).then((data) => {
       if (data.length == 0) {
-          return res.status(400).json({ error: 'couse does exist' });
+          return res.status(400).json({ error: 'course does exist' });
         }
         res.status(200).send(data);
   })
 };
 
+exports.getLesson = (req, res) => {
+  const { courseId, lessonNumber } = req.params
+  db('videos').where({ courseId, number: lessonNumber }).first().then((data) => {
+    if (data.length === 0) {
+      return res.status(400).json({ error: 'course does not exist'});
+    }
+    return res.status(200).send(data);
+  });
+};
+
+exports.getLessonList = (req, res) => {
+  const { courseId } = req.params;
+  db('videos').where({ courseId }).then((data) => {
+    if (data.length === 0) {
+      return res.status(400).json({ error: 'course does not exist or is empty'});
+    }
+    const sortedData = data.sort((a,b) => (
+      a.number > b.number ? 1 : -1
+    ));
+    return res.status(200).send(sortedData);
+  });
+};
+
+exports.createVideoLesson = async (req, res) => {
+  const { body } = req;
+  db('videos').insert({ ...body, id: `${body.courseId}-${body.number}` }).then((data) => {
+    console.log(data);
+    res.status(201).send({
+      ...body,
+      id: `${body.courseId}-${body.number}`,
+    });
+  });
+};
+
 exports.videoUpload = async (req, res) => {
-  const { id } = req.params;
-  await db('courses').where({ id }).first().update({ videoPath: req.file.path });
+  const { courseId, lessonNumber } = req.params;
+  await db('videos').where({ courseId, number: lessonNumber }).first().update({ videoPath: req.file.path });
   res.send('uploaded successfully');
 };
 
 exports.getVideo = async (req, res) => {
-  const { id } = req.params;
-  const movieFile = await db('courses').where({ id }).first(); //onde está salvo o video
-  console.log(movieFile.videoPath);
+  const { courseId, lessonNumber } = req.params;
+  const movieFile = await db('videos').where({ courseId, number: lessonNumber }).first(); //onde está salvo o video
+  if (!movieFile || !movieFile.videoPath) { return res.status(404).end('<h1>Movie Not Found</h1>')}
   fs.stat(movieFile.videoPath, (err, stats) => {
     if (err) {
       console.log(err);
@@ -82,4 +116,30 @@ exports.getVideo = async (req, res) => {
     stream.on('error', (streamErr) => res.end(streamErr));
     return null;
   });
+  return null;
+};
+
+exports.checkProgress = (req, res) => {
+  const { courseId, userId } = req.params;
+  db('progress').where({ courseId, userId }).first().then((data) => {
+    if (!data) {
+      return res.status(200).send({ lastSeen: 0 });
+    }
+    return res.status(200).send({ lastSeen: data.lastSeen || 0 });
+  });
+};
+
+exports.updateProgress = async (req, res) => {
+  const { courseId, userId } = req.params;
+  const { body } = req;
+  const data = await db('progress').where({ courseId, userId }).first();
+  if (!data) {
+    console.log('criado');
+    await db('progress').insert({
+      courseId, userId, lastSeen: body.lastSeen, id: `${userId}-${courseId}`,
+    });
+  } else {
+    await db('progress').where({ courseId, userId }).first().update({ lastSeen: body.lastSeen });
+  }
+  return res.status(200).send('progress updated');
 };
--- src/Routes/CoursesRoute.js
+++ src/Routes/CoursesRoute.js
@@ -2,15 +2,20 @@ const fs = require('fs');
 const CoursesController = require('../Controllers/CoursesController');
 const upload = require('../../common');
 
-module.exports = (app) =>{
+module.exports = (app) => {
    app.get('/', (req, res) => {
       fs.readFile('./index.html', (err, html) => res.end(html));
    });
    app.post('/course', CoursesController.post);
+   app.post('/course/video', CoursesController.createVideoLesson);
    app.put('/course/:id', CoursesController.put);
    app.delete('/course/:id', CoursesController.delete);
    app.get('/courses', CoursesController.get);
    app.get('/course/:id', CoursesController.getById);
-   app.post('/courses/upload/:id', upload.single('video'), CoursesController.videoUpload);
-   app.get('/courses/:id', CoursesController.getVideo);
+   app.get('/courses/list/:courseId', CoursesController.getLessonList);
+   app.get('/courses/video/:courseId/:lessonNumber', CoursesController.getLesson);
+   app.post('/courses/upload/:courseId/:lessonNumber', upload.single('video'), CoursesController.videoUpload);
+   app.get('/courses/progress/:courseId/:userId', CoursesController.checkProgress);
+   app.post('/courses/progress/:courseId/:userId', CoursesController.updateProgress);
+   app.get('/courses/:courseId/:lessonNumber', CoursesController.getVideo);
 };
